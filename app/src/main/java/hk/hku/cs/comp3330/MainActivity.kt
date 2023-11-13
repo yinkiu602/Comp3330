c6d6d9e2d1cc15dfd0e7d31506c1d3c4b680a867
yinkiu602 <josh@Joshwongyingkiu.com>
2023-11-13 12:55:50 +0800

    Initial commit

---
+++ app/src/main/java/hk/hku/cs/comp3330/MainActivity.kt
@@ -0,0 +1,11 @@
+package hk.hku.cs.comp3330
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
