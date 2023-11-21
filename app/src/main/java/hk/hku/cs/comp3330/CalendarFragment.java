018d45b4b7f1396c6f880961969c0696d25f4f0b
Thomas <u3578751@connect.hku.hk>
2023-11-21 11:04:31 +0800

    need debug

--- app/src/main/java/hk/hku/cs/comp3330/CalendarFragment.java
+++ app/src/main/java/hk/hku/cs/comp3330/CalendarFragment.java
@@ -8,11 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CalendarFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class CalendarFragment extends Fragment {
 
 
